d09e405ce53422f0eed56e4d4d226d3f9c1cfa79
tejas <tejaschendekar@yahoo.com>
2025-11-25 14:02:04 +0000

    Added some more refactoring

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socketio
 from aiohttp import web
-from dotenv import load_dotenv
 
 from logger import aeolus_logger
 from main.api.routes import setup_routes
@@ -8,21 +7,21 @@ from main.config import config
 from main.events.socket import setup_socket_events
 from main.redis import redis_client
 
-load_dotenv()
 
-# TODO This needs to be refactored
-if config.cors_origin == "*":
-    cors_allowed_origins = "*"
-else:
-    cors_allowed_origins = [*config.cors_origin.split(",")]
+def _setup_app():
+    if config.cors_origin == "*":
+        cors_allowed_origins = "*"
+    else:
+        cors_allowed_origins = [*config.cors_origin.split(",")]
 
-sio = socketio.AsyncServer(
-    async_mode="aiohttp", cors_allowed_origins=cors_allowed_origins, logger=True, engineio_logger=True
-)
+    sio = socketio.AsyncServer(
+        async_mode="aiohttp", cors_allowed_origins=cors_allowed_origins, logger=True, engineio_logger=True
+    )
+    setup_socket_events(sio, config.auth_token_prefix)
+    app = web.Application()
+    setup_routes(app)
 
-app = web.Application()
-setup_socket_events(sio, config.auth_token_prefix)
-setup_routes(app)
+    return app
 
 
 async def on_startup(app):
@@ -36,9 +35,10 @@ async def on_shutdown(app):
     await redis_client.disconnect()
 
 
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
-
-
 if __name__ == "__main__":
+    app = _setup_app()
+
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
     web.run_app(app, host="0.0.0.0", port=config.port)
